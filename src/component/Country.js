1fdb60da1357c8083cb3261eed75ef05952ccb80
Ritesh Agrahari <60784540+ritesh25033@users.noreply.github.com>
2025-06-19 08:58:50 +0530

    Update Country.js

--- src/component/Country.js
+++ src/component/Country.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 
 const Country = () => {
   const [countries, setCountries] = useState([]);
-  // const [error, setError] = useState(null);
 
   const fetchCountries = async () => {
     try {
